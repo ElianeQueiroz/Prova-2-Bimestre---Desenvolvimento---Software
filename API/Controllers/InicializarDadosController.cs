7fc8224a7de7ba3d2777234231b4fda6b569313f
ElianeQueiroz <elianeleyanne@hotmail.com>
2021-11-24 19:26:13 -0300

    commit

--- API/Controllers/InicializarDadosController.cs
+++ API/Controllers/InicializarDadosController.cs
@@ -21,14 +21,16 @@ namespace API.Controllers
         {
             _context.Categorias.AddRange(new Categoria[]
                 {
-                    new Categoria { CategoriaId = 1, Nome = "Categoria 1" },
+                    new Categoria { CategoriaId = 1, Nome = "Periféricos" },
+                    new Categoria { CategoriaId = 2, Nome = "Notebook" },
+                    new Categoria { CategoriaId = 3, Nome = "Desktop" },
                 }
             );
             _context.Produtos.AddRange(new Produto[]
                 {
-                    new Produto { ProdutoId = 1, Nome = "Produto 1", Preco = 1, Quantidade = 1, CategoriaId = 1 },
-                    new Produto { ProdutoId = 2, Nome = "Produto 2", Preco = 2, Quantidade = 2, CategoriaId = 1 },
-                    new Produto { ProdutoId = 3, Nome = "Produto 3", Preco = 3, Quantidade = 3, CategoriaId = 1 },
+                    new Produto { ProdutoId = 1, Nome = "Mouse", Preco = 50, Quantidade = 100, CategoriaId = 1 },
+                    new Produto { ProdutoId = 2, Nome = "Notebook DELL", Preco = 3500, Quantidade = 10, CategoriaId = 2 },
+                    new Produto { ProdutoId = 3, Nome = "Desktop DELL", Preco = 3000, Quantidade = 5, CategoriaId = 3 },
                 }
             );
             _context.SaveChanges();
